db54cb8299ea3c9d748efe042f50a0e67c582226
joelabraham <z5310056@ad.unsw.edu.au>
2023-05-10 12:40:24 +1000

    created basic react app with a header bar

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="/d" className="logo">MyBlog</a>
+        <nav>
+          <a href="/a">Login</a>
+          <a href="/a">Register</a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
