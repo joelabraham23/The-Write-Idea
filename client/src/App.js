bd68e3132bab7035eb534a6a12c4a007baba3ffd
joelabraham <z5310056@ad.unsw.edu.au>
2023-05-11 12:42:20 +1000

    confidgured display of each post and included author name etc

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,28 @@ function App() {
           <a href="/a">Register</a>
         </nav>
       </header>
+      <div className="post">
+        <div className="image">
+          <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt="blog post"></img>
+        </div>
+        <div className="texts">
+          <h2>Full house blah blah blah</h2>
+          <p className='info'>
+            <span className="author">Joel Abraham</span>
+            <time>11/05/2023</time>
+          </p>
+          <p>lerum ipsum dolor nfjfnjwn wefbwfjbwjfwf wkhfbhjwbf w nw h fhwbf</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt="blog post"></img>
+        </div>
+        <div className="texts">
+          <h2>Full house blah blah blah</h2>
+          <p>lerum ipsum dolor nfjfnjwn wefbwfjbwjfwf wkhfbhjwbf w nw h fhwbf</p>
+        </div>
+      </div>
     </main>
   );
 }
